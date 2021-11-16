5969e07712c607e3a5b7f5a1b3633f31b6e547ee
Alex <grepolisiert@hotmail.de>
2021-11-16 20:17:52 +0000

    #5 add Author model and create, show method to controller

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -1,6 +1,6 @@
 describe "Author model", type: :model do
     it "should exist a author model having 'first_name', 'last_name', and 'homepage' as well as a method 'name', which returns the full name of an author" do
-        author = Author.new('Alan', 'Turing', 'http://wikipedia.org/Alan_Turing')
+        author = Author.new(first_name: 'Alan', last_name: 'Turing', homepage: 'http://wikipedia.org/Alan_Turing')
         expect(author.first_name).to eq('Alan')
         expect(author.last_name).to eq('Turing')
         expect(author.homepage).to eq('http://wikipedia.org/Alan_Turing')
