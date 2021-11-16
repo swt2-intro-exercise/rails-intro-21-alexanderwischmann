ce7de0a7543f85d732e85682308379c88ec0b28b
Alex <grepolisiert@hotmail.de>
2021-11-16 20:50:44 +0000

    #6 add authors index page

--- spec/features/author/authors_index_spec.rb
+++ spec/features/author/authors_index_spec.rb
@@ -4,8 +4,7 @@ describe "Authors index page", type: :feature do
     it "should contain an HTML table with the headings 'Name' and 'Homepage', listing the full names as well as homepages of authors and should link to individual author's detail pages. The page should also contain a link to add new authors." do
         visit authors_path
         
-        expect(page).to have_field('author[name]')
-        expect(page).to have_field('author[homepage]')
+        expect(page).to have_table
         expect(page).to have_link 'New', href: new_author_path
     end
 end
