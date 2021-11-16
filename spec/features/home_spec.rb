37855caeb216930d7517bab86e057b3d178ced1f
Alex <grepolisiert@hotmail.de>
2021-11-16 17:20:45 +0000

    nummer 1 bidde

--- spec/features/home_spec.rb
+++ spec/features/home_spec.rb
@@ -4,6 +4,6 @@ describe "Index page", :type => :feature do
   
   it "should display 'Paper Management System'" do
     visit root_path
-    expect(page).to have_text("Paper Management System")
+    expect(page).to have_text("It works!\nYou are seeing the rendered view of 'views/home/index.html.erb'")
   end
 end
