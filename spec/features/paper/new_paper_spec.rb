bfa1e967dfa99fdc0e6f5b94d3f9da420def9204
Alex <grepolisiert@hotmail.de>
2021-11-21 17:45:45 +0000

    #10 add test case new paper

---
+++ spec/features/paper/new_paper_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+describe "New paper page", type: :feature do
+  it "should exist at 'new_paper_path' and render withour error" do
+    visit new_paper_path
+  end
+  it "should have a 'title' and a 'venue' string as well as a 'year' integer, indicating publishing date" do
+    visit new_paper_path
+    expect(page).to have_field('paper[title]')
+    expect(page).to have_field('paper[venue]')
+    expect(page).to have_field('paper[year]')
+  end
+end
